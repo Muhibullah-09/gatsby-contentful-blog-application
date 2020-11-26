03c531b0b164be252bebac3bc40ad3ea8aaa3480
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-11-26 07:58:21 +0500

    add contentful with gatsby

--- gatsby-contentful-blogsite/gatsby-config.js
+++ gatsby-contentful-blogsite/gatsby-config.js
@@ -1,11 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog Site`,
+    description: `Write your site description here!`,
+    author: `@ibaslogic`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: "l67fdi1zcd0i",
+        accessToken: "ATOw8qV46itZo_udjA2DuhWcEQTcVWDJG8qGDDWzXz0",
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
